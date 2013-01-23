c48cd096fc4f12d36b2bee3a85236db2d9cc5219
kiripaul <kiripaul@gmail.com>
2013-01-23 11:28:41 -0500

    MatrixPerturb
    Created MatrixPerturb() function.  Also, added import copy statement at
    the top of script and used deepcopy() in matrix perturb

--- Serial_Hill_Climber.py
+++ Serial_Hill_Climber.py
@@ -2,6 +2,7 @@ import numpy
 from scipy import *
 import matplotlib.pyplot as plt
 import random
+import copy
 
 def MatrixCreate(Rows,Columns):
     matrix_list = numpy.zeros(shape=(Rows,Columns))
@@ -27,6 +28,7 @@ def Fitness(Array_Name):
     #element_size determines the number of columns in each row by dividing the total number of items in the array/number of rows
     row = 0
     vector_means = MatrixCreate(array_size,1)
+    #vector_means will hold the means of each row: it is set up so that it can work with multidimensional matrices
     num_elem = 0
     while(row<array_size):
         for column in range(element_size):
@@ -36,3 +38,25 @@ def Fitness(Array_Name):
         vector_means[row,0] = vector_mean
         row = row+1
     return vector_means
+
+def MatrixPerturb(Array_Name,Probability):
+    random.seed()
+    array_size = len(Array_Name)
+    #len() gives the number of rows
+    element_size = (size(Array_Name))/array_size
+    #element_size determines the number of columns in each row by dividing the total number of items in the array/number of rows
+    prob_vector = MatrixCreate(array_size,element_size)
+    prob_vector = copy.deepcopy(Array_Name)
+
+    xx = random.random()
+    yy = size(p)
+    xy = 0
+
+    while xy<yy:
+        if(xx<Probability):
+            p[xy] = random.random()
+            
+        xx = random.random()
+        xy += 1
+        
+    return p
