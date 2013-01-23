7c04fc7cf4836e08ae38a4a23eb9627e2e322c81
kiripaul <kiripaul@gmail.com>
2013-01-23 10:43:44 -0500

    Fitness()
    Added a fitness function to sum up all of the elements of a vector and
    take the mean of the sum.

--- Serial_Hill_Climber.py
+++ Serial_Hill_Climber.py
@@ -18,11 +18,33 @@ def MatrixRandomize(ArrayName):
 def MatrixRandomize(Array_Name):
     random.seed()
     array_size = len(Array_Name)
+    #len() gives the number of rows
     element_size = (size(Array_Name))/array_size
-    #element size determines the number of columns in each row by dividing the total number of items in the array/number of rows
+    #element_size determines the number of columns in each row by dividing the total number of items in the array/number of rows
     row = 0
     while(row<array_size):
         for column in range(element_size):
             Array_Name[row,column] = random.random()
         row = row+1
     return Array_Name
+
+def Fitness(Array_Name):
+    array_size = len(Array_Name)
+    #len() gives the number of rows
+    element_size = (size(Array_Name))/array_size
+    #element_size determines the number of columns in each row by dividing the total number of items in the array/number of rows
+    row = 0
+    run_sum = 0
+    current_element = 0
+    #vector_means = MatrixCreate(array_size,element_size)
+    vector_mean=0
+    num_elem = 0
+    while(row<array_size):
+        for column in range(element_size):
+            current_element = Array_Name[row,column]#grabbing the current value
+            num_elem = num_elem + 1
+            run_sum = run_sum + current_element#summing up the current and previous elements
+            #vector_means[row,column] = run_sum
+        vector_mean = run_sum/num_elem
+        row = row+1
+    return vector_mean
